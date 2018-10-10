509d867728fa7414a2bd5dcd749eab31a4cced83
camilaleniss <mlmariacami2@gmail.com>
2018-10-10 15:05:41 -0500

    Generados métodos añadir y buscar BinaryTree

--- U3-Lenis-Palma-Torres/src/main/java/model/BinaryTree.java
+++ U3-Lenis-Palma-Torres/src/main/java/model/BinaryTree.java
@@ -1,21 +1,22 @@
 package model;
 
-public class BinaryTree<T> implements IBinaryTree {
+public class BinaryTree<T> implements IBinaryTree<T> {
 
-	public void addNode(Object node) {
+	public void addNode(T node) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	public NodeBinaryTree deleteNode(Object node) {
+	public NodeBinaryTree<T> deleteNode(T node) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public NodeBinaryTree searchNode(Object node) {
+	public NodeBinaryTree<T> searchNode(T node) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 
 	
 
--- U3-Lenis-Palma-Torres/src/main/java/model/NodeBinaryTree.java
+++ U3-Lenis-Palma-Torres/src/main/java/model/NodeBinaryTree.java
@@ -1,6 +1,6 @@
 package model;
 
-public class NodeBinaryTree<T> {
+public class NodeBinaryTree<T> implements Comparable{
 
 	private T value;
 	private NodeBinaryTree<T> parent;
@@ -15,6 +15,10 @@ public class NodeBinaryTree<T> {
 	
 	public T getValue() {
 		return value;
+	}
+	
+	public void setValue(T value) {
+		this.value=value;
 	}
 
 	public NodeBinaryTree<T> getRight() {
@@ -35,6 +39,57 @@ public class NodeBinaryTree<T> {
 
 	public boolean isLeaf() {
 		return (left==null && right==null);
+	}
+
+	//REVISAR ESTE MÉTODO
+	public int compareTo(Object arg0) {
+		NodeBinaryTree node = (NodeBinaryTree) arg0;
+		int v = (Integer) node.getValue();
+		return (Integer)value-v;
+	}
+	
+	public void addNode (T node) {
+		NodeBinaryTree<T> toAdd = new NodeBinaryTree<T>(node);
+		if (compareTo(toAdd)>=0) {
+			if (left == null)
+				left = toAdd;
+			else
+				left.addNode(node);
+		}else {
+			if (right==null)
+				right = toAdd;
+			else
+				right.addNode(node);
+		}
+	}
+	
+	public T searchValue(T value) {
+		if (this.value == value) 
+			//VER ESTO
+			return (T) this;
+		if (isLeaf())
+			return null;
+		if (compareTo(value)>=0)
+			return (left!=null) ? left.searchValue(value) : null;
+		else
+			return (right!=null) ? right.searchValue(value) : null;	
+	}
+	
+	public T delete(T value) {
+		//Falta implementarlo
+		return value;
+	}
+	
+	public T findMin() {
+		return (T) ((left==null) ? this : left.findMin());
+	}
+	
+	public T findMax() {
+		return (T) ((right==null) ? this : right.findMin());
+	}
+	
+	public T findSuccesor() {
+		return (T) this;
 	}
 	
 }
