28ba42088ea1a2e591a248c25d5bf6d953f65bb2
camilaleniss <mlmariacami2@gmail.com>
2018-10-12 12:51:37 -0500

    Creada interfaz

---
+++ U3-Lenis-Palma-Torres/src/main/java/controller/MainController.java
@@ -0,0 +1,19 @@
+package controller;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+
+public class MainController {
+
+    @FXML
+    private MenuButton nabuconodosor;
+
+    @FXML
+    void maria(ActionEvent event) {
+System.out.println("sdf");
+    }
+
+}
+
