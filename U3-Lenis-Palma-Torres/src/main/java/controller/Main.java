3a0a1e73dd9c262d56a7fee02920d9a034289e49
Javier Torres <javierandrestorresreyes@gmail.com>
2018-10-22 22:05:56 -0500

    Traduccion gui y arreglo de que no ejecutaba

--- U3-Lenis-Palma-Torres/src/main/java/controller/Main.java
+++ U3-Lenis-Palma-Torres/src/main/java/controller/Main.java
@@ -1,9 +1,15 @@
 package controller;
 
+import java.io.File;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -15,8 +21,8 @@ public class Main extends Application{
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("FIBA");
+		primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/baloncesto.png")));
 		primaryStage.show();
-		
 	}
 	
 	public static void main(String[] args) {
